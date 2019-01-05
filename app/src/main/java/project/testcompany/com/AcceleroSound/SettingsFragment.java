78acad0d2369706995f1e545d16d1e72626ad1ae
YilmazKerem <s100757@ap.be>
2019-01-05 20:29:17 +0100

    Thb niks veel

--- app/src/main/java/project/testcompany/com/AcceleroSound/SettingsFragment.java
+++ app/src/main/java/project/testcompany/com/AcceleroSound/SettingsFragment.java
@@ -3,27 +3,37 @@ package project.testcompany.com.AcceleroSound;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.content.Context;
 
 import org.w3c.dom.Text;
 
 import project.testcompany.com.test1.R;
+import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class SettingsFragment extends Fragment{
     TextView textView;
     SeekBar seekBar;
 
     int min = 0, max = 80, current = 40;
-    //int CurrentVelocity = getResources().getInteger(R.integer.VelocitySpeed);
+
+    ConstraintLayout mLayout;
+    int mDefaultColor;
+    Button ButtonColor;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Nullable
@@ -37,11 +47,24 @@ public class SettingsFragment extends Fragment{
         seekBar.setOnSeekBarChangeListener(this);*/
 
 
+       /* mLayout = view.findViewById(R.id.Layout);
+        mDefaultColor = ContextCompat.getColor(new MainActivity(), R.color.ColorButton);
+        ButtonColor = (Button) view.findViewById(R.id.Color_Picker);
+        ButtonColor.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                openColorPicker();
+            }
+        });*/
+
         textView =  (TextView) view.findViewById(R.id.seektext);
         seekBar =  (SeekBar) view.findViewById(R.id.seekBar);
         seekBar.setMax(max - min);
         seekBar.setProgress(current);
         textView.setText(""+current);
+
+
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -62,6 +85,8 @@ public class SettingsFragment extends Fragment{
 
             }
         });
+
+
 
         return view;
 
